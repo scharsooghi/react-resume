f2972870ed74476fb4c661bc3e3967ab609bcc37
scharsooghi <scharsooghi@gmail.com>
2020-03-30 15:31:51 +0200

    Updates

---
+++ precache-manifest.f058377d92064ee4adb6d65941a5f172.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ddd81fa3bf13a39a245ffa0f9181290",
+    "url": "/react-resume/index.html"
+  },
+  {
+    "revision": "4f8b707fa8f05a2b3406",
+    "url": "/react-resume/static/css/main.6c4d3319.chunk.css"
+  },
+  {
+    "revision": "7224d3f29222d5c9ca4b",
+    "url": "/react-resume/static/js/2.4f63be87.chunk.js"
+  },
+  {
+    "revision": "e4d0b7b6ee221c9758b548593a6bc2ac",
+    "url": "/react-resume/static/js/2.4f63be87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f8b707fa8f05a2b3406",
+    "url": "/react-resume/static/js/main.c442b167.chunk.js"
+  },
+  {
+    "revision": "d749b574a47e12f96187",
+    "url": "/react-resume/static/js/runtime-main.6a460ec0.js"
+  }
+]);
